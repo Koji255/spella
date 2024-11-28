f2fac55c5da63c31cc18f1e0211a1a3612b87e01
Koji255 <koji25ony1@gmail.com>
2024-11-28 14:14:04 +0300

    styling for index page done

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/koji/Desktop/code/django/projects/speller/venv
+set -gx VIRTUAL_ENV /home/koji/Desktop/code/django_projects/speller/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
